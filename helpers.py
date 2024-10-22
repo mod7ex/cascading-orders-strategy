861a5031ea9bc523ac93e3cd611d3734ede4c5cf
Mourad EL CADI <mouradface1998@gmail.com>
2024-10-22 13:12:05 +0100

    save

--- helpers.py
+++ helpers.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def count_max_consecutive_losses(_series: pd.Series):
+def count_max_consecutive(_series: pd.Series, _condition):
     _max_count = 0
     _count = 0
 
     for _item in _series: # row for reading, i for writing
-        if _item < 0:
+        if _condition(_item):
             _count += 1
         else:
             if _count > _max_count:
